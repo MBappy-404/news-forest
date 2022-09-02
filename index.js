18c9d9e299f274287bb08cfcad6ad30f58ee9591
MBappy-404 <muhammadbappy989@gmail.com>
2022-09-02 23:56:46 +0530

    added category menu

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const loadCategoryes =async() =>{
-     const url = `https://openapi.programming-hero.com/api/news/categories`;
+const loadCategoryes = async() => {
 
+     const url = `https://openapi.programming-hero.com/api/news/categories`
+     fetch(url)
      const res = await fetch(url);
      const data = await res.json();
+
      seatAllMenu(data.data);
 
 }
+const seatAllMenu = category => {
 
 
-const seatAllMenu = category =>{
-
-     console.log(category.news_category[0].category_name);
-
-
+     const categoryName = document.getElementById('all-menu');
+     category.news_category.forEach(element => {
+          
+          const categoryList = document.createElement('list');
+          categoryList.innerHTML = `
+          <li class="nav-item">
+            <a class="nav-link "href="#"> ${element.category_name}</a>
+           </li>`;
+          
+           categoryName.appendChild(categoryList);
+     });
 }
 
 loadCategoryes();
